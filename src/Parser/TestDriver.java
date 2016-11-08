797e503f26e5ef50056b8e157fa8f5a8c6b2121e
ChenKuo <ckuo1@umbc.edu>
2016-11-08 13:31:17 -0500

    fix driver

--- src/Parser/TestDriver.java
+++ src/Parser/TestDriver.java
@@ -1,12 +1,11 @@
 package Parser;
-import java.util.*;
 
 public class TestDriver {
 	public static void main(String[] args){
 		Color[][] color= new Color[10][10];
 		for(int i=0; i<10; i++){
 			for(int j=0; j<10; j++){
-				color[i][j]=new Color(i*10+j);
+				color[i][j]=new Color(i*10+j,0,0);
 			}
 		}
 		
@@ -15,7 +14,7 @@ public class TestDriver {
 		System.out.println("original:");
 		for(int i=0; i<10; i++){
 			for(int j=0; j<10; j++){
-				System.out.print(pic.pixels[i][j].x+" ");
+				System.out.print(pic.pixels[i][j].r+" ");
 			}
 			System.out.println();
 		}
@@ -25,7 +24,7 @@ public class TestDriver {
 		System.out.println("sub:");
 		for(int i=0; i<5; i++){
 			for(int j=0; j<5; j++){
-				System.out.print(subpic.pixels[i][j].x+" ");
+				System.out.print(subpic.pixels[i][j].r+" ");
 			}
 			System.out.println();
 		}
